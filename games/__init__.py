f582cc647a076f85fcce1dd9c9353d224ae9e0d1
hanotsosall <dmitrijkucenko332@gmail.com>
2026-05-28 21:21:04 +0300

    Create __init__.py

---
+++ games/__init__.py
@@ -0,0 +1,7 @@
+from .slots import spin as slots_spin
+from .mines import generate, reveal
+from .crash import start, cashout, get_status
+from .dice import roll
+from .coinflip import flip
+from .keno import draw
+from .boomcity import spin
