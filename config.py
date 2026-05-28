ddd28eb8f753222a32020953ff626c525971ccbc
hanotsosall <dmitrijkucenko332@gmail.com>
2026-05-28 20:12:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = 'cashx_super_secret_2025'
-    MIN_WITHDRAWAL = 50
-    BONUS_ON_REGISTER = 5000
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'ultimate-cashx-secret-key-2025')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///cashx_ultimate.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MIN_WITHDRAWAL = 1000   # минимальный вывод в рублях
+    # Платёжные ключи (заглушки)
+    STRIPE_API_KEY = os.environ.get('STRIPE_API_KEY', '')
+    COINPAYMENTS_API_KEY = os.environ.get('COINPAYMENTS_API_KEY', '')
